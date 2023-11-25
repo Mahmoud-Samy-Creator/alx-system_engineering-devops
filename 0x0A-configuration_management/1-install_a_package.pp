00f24ea4fd09cde14e7c35c64e2526f7025b5981
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-25 00:05:13 +0000

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,10 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# A puppet manifest - Install Werkzeug from pip3
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
