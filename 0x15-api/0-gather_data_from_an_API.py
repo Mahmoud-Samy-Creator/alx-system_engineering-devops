d864bf9f52b571841df1c437d7b44e1a0e17e0fa
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-03 09:40:53 -0500

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,25 +6,25 @@ import requests
 import json
 from sys import argv
 
-employer_id = int(argv[1])
-no_of_done_tasks = 0
+if __name__ == "__main__":
+    id = int(argv[1])
+    done = 0
 
-users_response = requests.get("https://jsonplaceholder.typicode.com/users")
-todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    users_response = requests.get("https://jsonplaceholder.typicode.com/users")
+    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-user_data = users_response.json()
-todos_data = todos_response.json()
+    user_data = users_response.json()
+    todos_data = todos_response.json()
 
+    for data in todos_data:
+        if data['userId'] == id:
+            if data['completed'] == 1:
+                done += 1
 
-for data in todos_data:
-    if data['userId'] == employer_id:
-        if data['completed'] == 1:
-            no_of_done_tasks += 1
+    print(f"Employee {user_data[id-1]['name']} is done with tasks({done}/20):")
 
-print(f"Employee {user_data[employer_id - 1]['name']} is done with tasks({no_of_done_tasks}/20):")
-
-for data in todos_data:
-    if data['userId'] == employer_id:
-        if data['completed'] == 1:
-            print("\t ", end="")
-            print(data['title'])
+    for data in todos_data:
+        if data['userId'] == id:
+            if data['completed'] == 1:
+                print("\t ", end="")
+                print(data['title'])
